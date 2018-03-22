294c2082c8bb3c954b3b2f199fa634e9123cc7ad
lmw1231 <liu_mingwei@163.com>
2018-03-22 15:56:21 +0800

    rollback

--- src/main/java/com/lmw/domain/TAntiThunder.java
+++ src/main/java/com/lmw/domain/TAntiThunder.java
@@ -10,8 +10,6 @@ public class TAntiThunder extends BaseBean implements Serializable {
 
 	private Integer id;
 
-	private String test;
-
 	private Integer pid;
 
 	private String jiancedianmingcheng;
